3d19d48fe4313228f9b7e19b0248a3530f798cd3
Senior S <maximatador23@gmail.com>
2020-11-30 18:20:56 -0300

    Full Version

--- SimpleClock.cs
+++ SimpleClock.cs
@@ -1,199 +1,72 @@
 ﻿using System;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using Cysharp.Threading.Tasks;
 using OpenMod.Unturned.Plugins;
 using OpenMod.API.Plugins;
 using SDG.Unturned;
-using System.Threading.Tasks;
 using OpenMod.Core.Helpers;
+using UnityEngine;
 
 [assembly: PluginMetadata("SS.SimpleClock", DisplayName = "SimpleClock")]
 namespace SimpleClock
 {
     public class SimpleClock : OpenModUnturnedPlugin
     {
-        private readonly IConfiguration m_Configuration;
-        private readonly IStringLocalizer m_StringLocalizer;
         private readonly ILogger<SimpleClock> m_Logger;
 
         internal bool Active = false;
-        internal uint ActualHour = 00;
-        internal uint ActualMinute = 00;
-
-        internal int lev = 0;
+        internal byte lev = 0;
 
         public SimpleClock(
-            IConfiguration configuration, 
-            IStringLocalizer stringLocalizer,
-            ILogger<SimpleClock> logger, 
+            ILogger<SimpleClock> logger,
             IServiceProvider serviceProvider) : base(serviceProvider)
         {
-            m_Configuration = configuration;
-            m_StringLocalizer = stringLocalizer;
             m_Logger = logger;
         }
 
         protected override async UniTask OnLoadAsync()
         {
-            // await UniTask.SwitchToMainThread();
             Active = true;
             m_Logger.LogInformation("Plugin loaded correctly!");
             Level.onLevelLoaded += OnLevelLoaded;
-			// await UniTask.SwitchToThreadPool();
         }
 
-        private void OnLevelLoaded(int level)
+        private void OnLevelLoaded(int level) //When the plugin is loaded this event is triggered (I don't know why) but for avoid problems i added a simple check with a byte.
         {
             lev++;
             if (lev == 2)
             {
-                AsyncHelper.Schedule("Clock Update", () => MinutesHelper().AsTask());
-                AsyncHelper.Schedule("Clock Update", () => Clock().AsTask());
+                AsyncHelper.Schedule("Log Time", () => LogTime().AsTask());
             }
         }
 
-        public async UniTask Clock()
+        public async UniTask LogTime()
         {
             await UniTask.SwitchToMainThread();
             while (Active)
             {
                 await UniTask.DelayFrame(48, PlayerLoopTiming.Update);
-                var cycle = LightingManager.cycle;
-                var actualtime = LightingManager.time;
-                ActualHour = GetRealHour(actualtime, cycle);
-                EffectManager.sendUIEffect(20540, 540, true, ActualHour.ToString(), ActualMinute.ToString());
-            }
-        }
+                float num = LightingManager.day / LevelLighting.bias;
 
-        public async UniTask MinutesHelper()
-        {
-            await UniTask.SwitchToMainThread();
-            LightingManager.time = 0;
-            int i = 0;
-            while (Active)
-            {
-                await UniTask.DelayFrame(48, PlayerLoopTiming.Update);
-                i++;
-                if (i <= (LightingManager.cycle / 24) - 1)
+                var dhour = Math.Floor(num * 12f);
+                string digitalHour = (dhour + 6).ToString("00");
+                var dminute = Math.Floor(((num * 12f) % 1f) * 181f);
+                var dminh = 181f / 60f;
+                string digitalMinute = Math.Floor(dminute / dminh).ToString("00");
+
+                if ((dhour + 6) > 12)
                 {
-                    i = 0;
+                    digitalHour = (dhour - 6).ToString("00");
                 }
-                
-            }
-        }
 
-        //private uint GetActualMinute(uint ActualTime)
-        //{
-
-        //}
-
-        private uint GetRealHour(uint ActualTime, uint Cycle)
-        {
-            
-            if (ActualTime >= 0 && ActualTime < Cycle / 24)
-            {
-                return 05;
+                EffectManager.sendUIEffect(32521, 521, true, $"{digitalHour}:{digitalMinute}");
             }
-            else if (ActualTime >= Cycle / 24 && ActualTime < Cycle / 23)
-            {
-                return 06;
-            }
-            else if (ActualTime >= Cycle / 23 && ActualTime < Cycle / 22)
-            {
-                return 07;
-            }
-            else if (ActualTime >= Cycle / 22 && ActualTime < Cycle / 21)
-            {
-                return 08;
-            }
-            else if (ActualTime >= Cycle / 21 && ActualTime < Cycle / 20)
-            {
-                return 09;
-            }
-            else if (ActualTime >= Cycle / 20 && ActualTime < Cycle / 19)
-            {
-                return 10;
-            }
-            else if (ActualTime >= Cycle / 19 && ActualTime < Cycle / 18)
-            {
-                return 11;
-            }
-            else if (ActualTime >= Cycle / 18 && ActualTime < Cycle / 17)
-            {
-                return 12;
-            }
-            else if (ActualTime >= Cycle / 17 && ActualTime < Cycle / 16)
-            {
-                return 13;
-            }
-            else if (ActualTime >= Cycle / 16 && ActualTime < Cycle / 15)
-            {
-                return 14;
-            }
-            else if (ActualTime >= Cycle / 15 && ActualTime < Cycle / 14)
-            {
-                return 15;
-            }
-            else if (ActualTime >= Cycle / 14 && ActualTime < Cycle / 13)
-            {
-                return 16;
-            }
-            else if (ActualTime >= Cycle / 13 && ActualTime < Cycle / 12)
-            {
-                return 17;
-            }
-            else if (ActualTime >= Cycle / 12 && ActualTime < Cycle / 11)
-            {
-                return 18;
-            }
-            else if (ActualTime >= Cycle / 11 && ActualTime < Cycle / 10)
-            {
-                return 19;
-            }
-            else if (ActualTime >= Cycle / 10 && ActualTime < Cycle / 9)
-            {
-                return 20;
-            }
-            else if (ActualTime >= Cycle / 9 && ActualTime < Cycle / 8)
-            {
-                return 21;
-            }
-            else if (ActualTime >= Cycle / 8 && ActualTime < Cycle / 7)
-            {
-                return 22;
-            }
-            else if (ActualTime >= Cycle / 7 && ActualTime < Cycle / 6)
-            {
-                return 23;
-            }
-            else if (ActualTime >= Cycle / 6 && ActualTime < Cycle / 5)
-            {
-                return 00;
-            }
-            else if (ActualTime >= Cycle / 5 && ActualTime < Cycle / 4)
-            {
-                return 01;
-            }
-            else if (ActualTime >= Cycle / 4 && ActualTime < Cycle / 3)
-            {
-                return 02;
-            }
-            else if (ActualTime >= Cycle / 3 && ActualTime < Cycle / 2)
-            {
-                return 03;
-            }
-            else if (ActualTime >= Cycle / 2 && ActualTime < Cycle / 1)
-            {
-                return 04;
-            }
-            return 00;
         }
 
         protected override async UniTask OnUnloadAsync()
         {
-            // await UniTask.SwitchToMainThread();
+            Level.onLevelLoaded -= OnLevelLoaded;
+            lev = 0;
             Active = false;
             m_Logger.LogInformation("Plugin unloaded correctly!");
         }
